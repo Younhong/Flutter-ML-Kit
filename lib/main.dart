145563b03622b529bf87a908da1411b617c8f407
Younhong <younhong@kakao.com>
2020-09-04 16:12:45 +0900

    upload

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+
+import 'camera.dart';
+
+// Global variable for storing the list of
+// cameras available
+List<CameraDescription> cameras = [];
+
+Future<void> main() async {
+  try {
+    WidgetsFlutterBinding.ensureInitialized();
+    // Retrieve the device cameras
+    cameras = await availableCameras();
+  } on CameraException catch (e) {
+    print(e);
+  }
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ML Vision',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: CameraScreen(cameras),
+    );
+  }
+}
